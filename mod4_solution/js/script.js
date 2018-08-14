f2803046961bfaebf40bab427eaaaeae3d654b8c
Amy P <amydpp@gmail.com>
2018-08-14 11:45:59 -0400

    Update script.js

--- mod4_solution/js/script.js
+++ mod4_solution/js/script.js
@@ -1,9 +1,38 @@
-(function (iffy) {
+(function (window) {
   var sillyNames = {};
   sillyNames.name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-//   var greeting = "Hello ";
-//   sillyNames.sayYes = function () {
-//     console.log(greeting + sillyNames.name);
-// }
-    iffy.sillyNames = sillyNames;
-  }) (iffy);
+
+
+ window.sillyNames = sillyNames;
+  }) (window);
+
+byeSpeaker.speak(sillyNames.name);
+helloSpeaker.speak(sillyNames.name);
+
+
+for (var i = 0; i < sillyNames.name.length -1; i++){
+console.log(helloSpeaker.speak(sillyNames.name[i]));
+
+};
+
+
+for (var i = 0; i < sillyNames.name.length -1; i++){
+
+console.log(byeSpeaker.speak(sillyNames.name[i]));
+
+
+
+};
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
